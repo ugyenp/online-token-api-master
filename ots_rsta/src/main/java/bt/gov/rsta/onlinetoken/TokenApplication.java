04dcb34199ef5e6bd574c7e04d0421b2a22ffc7b
ecentric@ngn.bt <ugyen penjor@ugyenpenjor.ngn.local>
2020-09-30 01:14:16 +0600

    changes

--- ots_rsta/src/main/java/bt/gov/rsta/onlinetoken/TokenApplication.java
+++ ots_rsta/src/main/java/bt/gov/rsta/onlinetoken/TokenApplication.java
@@ -2,10 +2,15 @@ package bt.gov.rsta.onlinetoken;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//lol
 @SpringBootApplication
 public class TokenApplication {
 
+	/*
+	 * Author: Ugyen Penjor 
+	 * Designation: Software Engineer 
+	 * NGN Technologies Pvt Ltd @ 2020
+	 */
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TokenApplication.class, args);
 	}
