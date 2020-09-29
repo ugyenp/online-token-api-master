1c8043c2460640b2b296eaa34574e908c7555a09
ecentric@ngn.bt <ugyen penjor@ugyenpenjor.ngn.local>
2020-09-30 01:09:52 +0600

    new update

--- ots_rsta/src/main/java/bt/gov/rsta/onlinetoken/TokenApplication.java
+++ ots_rsta/src/main/java/bt/gov/rsta/onlinetoken/TokenApplication.java
@@ -2,7 +2,7 @@ package bt.gov.rsta.onlinetoken;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//lol
 @SpringBootApplication
 public class TokenApplication {
 
